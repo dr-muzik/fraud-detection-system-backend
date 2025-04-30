da8b2879c564b697be0418c1fa3c8be532cfe7c5
Wisdom Peters <drmusicpiano@gmail.com>
2025-04-30 16:09:27 +0100

    feat: setting up prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FraudModule } from './fraud/fraud.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [FraudModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
+// This service extends the PrismaClient and implements the OnModuleInit and OnModuleDestroy interfaces.
+// It connects to the database when the module is initialized and disconnects when the module is destroyed.
